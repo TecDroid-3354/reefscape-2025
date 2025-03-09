353f77999bcdf92105643a155cee2ab0926831a0
Ayimany <mooncatayimany@proton.me>
2025-03-09 17:41:09 -0600

    Create Joint SysId routine

--- src/main/java/net/tecdroid/subsystems/elevatorjoint/ElevatorJoint.kt
+++ src/main/java/net/tecdroid/subsystems/elevatorjoint/ElevatorJoint.kt
@@ -7,22 +7,29 @@ import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.TalonFX
 import com.ctre.phoenix6.signals.NeutralModeValue
 import edu.wpi.first.units.Units.Degrees
+import edu.wpi.first.units.Units.Volts
 import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.AngularVelocity
 import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.util.sendable.Sendable
 import edu.wpi.first.util.sendable.SendableBuilder
+import edu.wpi.first.wpilibj.RobotController
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
 import net.tecdroid.subsystems.util.generic.VoltageControlledSubsystem
 import net.tecdroid.subsystems.util.generic.WithAbsoluteEncoders
+import net.tecdroid.subsystems.util.identification.AngularSysIdRoutine
+import net.tecdroid.subsystems.wrist.Wrist
 import net.tecdroid.util.units.clamp
 import net.tecdroid.wrappers.ThroughBoreAbsoluteEncoder
 
 class ElevatorJoint(private val config: ElevatorJointConfig) : SubsystemBase(), Sendable, VoltageControlledSubsystem,
     WithAbsoluteEncoders {
-    private val leadMotorController = TalonFX(config.leadMotorControllerId.id)
+    internal val leadMotorController = TalonFX(config.leadMotorControllerId.id)
     private val followerMotorController = TalonFX(config.followerMotorId.id)
 
     private val absoluteEncoder =
@@ -49,8 +56,17 @@ class ElevatorJoint(private val config: ElevatorJointConfig) : SubsystemBase(), 
 
     fun setTargetAngleCommand(angle: Angle): Command = Commands.runOnce({ setTargetAngle(angle) })
 
+    internal val motorPosition: Angle
+        get() = leadMotorController.position.value
+
+    internal val motorVelocity: AngularVelocity
+        get() = leadMotorController.velocity.value
+
     val angle: Angle
-        get() = config.gearRatio.apply(leadMotorController.position.value)
+        get() = config.gearRatio.apply(motorPosition)
+
+    val angularVelocity: AngularVelocity
+        get() = config.gearRatio.apply(motorVelocity)
 
     private val absoluteAngle: Angle
         get() = config.gearRatio.apply(absoluteEncoder.position)
@@ -110,4 +126,19 @@ class ElevatorJoint(private val config: ElevatorJointConfig) : SubsystemBase(), 
     }
 }
 
+class ElevatorJointSystemIdentificationRoutine(joint: ElevatorJoint) : AngularSysIdRoutine() {
+    override val routine: SysIdRoutine = SysIdRoutine(
+        SysIdRoutine.Config(),
+        SysIdRoutine.Mechanism(
+            joint::setVoltage,
+            { log: SysIdRoutineLog ->
+                log.motor("Wrist Motor")
+                    .voltage(voltage.mut_replace(RobotController.getBatteryVoltage() * joint.leadMotorController.get(), Volts))
+                    .angularPosition(position.mut_replace(joint.motorPosition))
+                    .angularVelocity(velocity.mut_replace(joint.motorVelocity))
+            },
+            joint
+        )
+    )
+}
 
