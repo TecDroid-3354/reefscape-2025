fdedcf85cd9ecffa050dcbdfd29624d19c815e08
Santiago Fernando Murguia <santiagofernandomurguia@gmail.com>
2025-03-12 09:18:40 -0600

    NO USAR ESTE COMMIT. Solo para mergear métodos

--- src/main/java/net/tecdroid/core/RobotContainer.kt
+++ src/main/java/net/tecdroid/core/RobotContainer.kt
@@ -2,8 +2,10 @@ package net.tecdroid.core
 
 import edu.wpi.first.units.Units.Meters
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import net.tecdroid.constants.GenericConstants.driverControllerId
 import net.tecdroid.input.CompliantXboxController
@@ -18,12 +20,17 @@ import net.tecdroid.systems.arm.*
 import net.tecdroid.util.units.meters
 import net.tecdroid.util.units.rotations
 import net.tecdroid.util.units.volts
+import net.tecdroid.auto.AutoRoutines
+
 
 class RobotContainer {
     private val controller = CompliantXboxController(driverControllerId)
     private val swerve = SwerveSystem(swerveDriveConfiguration)
     private val arm = ArmSystem(wristConfig, elevatorConfig, elevatorJointConfig)
     private val intake = Intake(intakeConfig)
+
+    private val auto = AutoRoutines()
+//    private val autoChooser = AutoChooser()
 
     init {
         val tab = Shuffleboard.getTab("Robot Container")
@@ -87,7 +94,7 @@ class RobotContainer {
 
     }
 
-    val autonomousCommand: Command?
-        get() = null
 
+    val autonomousCommand: Command?
+        get() = null // kdsafd
 }
